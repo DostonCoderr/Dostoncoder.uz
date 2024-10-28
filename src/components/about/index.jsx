ece50adcc446c42903c809030d12760c92d52099
Doston <132774658+DostonCoderr@users.noreply.github.com>
2024-10-28 09:50:43 +0500

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -83,8 +83,7 @@ const AboutDetails = () => {
         </ItemLayout>
 
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-        <img  className="w-full h-auto" src="https://github-readme-streak-stats.herokuapp.com?user=https%3A%2F%2Fgithub.com%2FDostonCoderr&theme=dark&hide_border=true&exclude_days=Sun%2CMon%2CTue%2CWed%2CThu%2CFri%2CSat&card_width=500&card_height=203)](https://git.io/streak-stats"
-         loading="lazy"/>
+        <img src="https://github-readme-streak-stats.herokuapp.com?user=https%3A%2F%2Fgithub.com%2FDostonCoderr&theme=dark&hide_border=true&exclude_days=Sun%2CMon%2CTue%2CWed%2CThu%2CFri%2CSat&card_width=500&card_height=203" alt="GitHub Streak" />
         </ItemLayout>
 
       
